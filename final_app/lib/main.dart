c3f8e34c8e49e1313f6165c7bc26440472f997bd
unknown <dahlanrafli5@gmail.com>
2024-06-19 21:12:19 +0800

    final mobile

---
+++ final_app/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:final_app/splash.dart';
+import 'package:flutter/material.dart';
+import 'package:final_app/homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/home': (context) => const HomePage(),
+      },
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
